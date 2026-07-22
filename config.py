39ecf28817c12f8e59fedbde372f9692bbba9a38
Rdip <96578868+Rdipofficial@users.noreply.github.com>
2026-07-23 00:51:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ GROUP       = os.getenv("GROUP", "nub_coder_s")
 
 # ── Sensitive — must be set via environment, no defaults ────────────────────────
 BOT_TOKEN      = os.getenv("BOT_TOKEN", "8975766165:AAENEio15whYE0FmOObR8xVlFZ1x1hNsMto")
-STRING_SESSION = os.getenv("STRING_SESSION", "BQFGMU4AQYjaTl5KGFywai_fk4ILzgKy0h66BURPla6j92kyov_G2WtBBcKmzfdETS9fiQHM5QKc3J5azp16PGsyCiFu1wXvVJIE_ln4xHfQxxFr-cVku47xSMSYW4jHakdXZbqF2i_ia92nfNEt4vUXg80Hvh5rB76qwokhOIsd6YnpCUkfoa7700FgROyc0k1clNlD373JXGwVk9r3LhbbXws7aS67zdppk8k2rpTnxcJFewKUcTO3ztMQZffSFszodsxgJArLlWObrU_vuO5wnv366nj267zJdqThLrl_6mirIvSDpIegjGVoIa6130ZwS2t_-cdf-GZGV72O2Bjf-UxMygAAAAH-pi3FAA")
+STRING_SESSION = os.getenv("STRING_SESSION", "BQFGMU4AoJIoI576_9ctxVTWo2Iy8YkRqtMfxKiv22vwlpzz2xE_I5_ZRuCGCEMILYiv5Q-kTtLirqT4GorSN1h7XHETnAwFuvxUVgmVygAHr2P-CWT0cFl3qfN_uDMFkUI9xRdB8EG1t6gpaXMbbzIcNWTBdb6UzJLngFTt6M8q1aK0OEtI72STkt--cvKSlxDI1vSKnxfp4vCx7uqzwFcykpay1ExkjulHmMlRY90g6dHvCuvwF2AvO0ujK5kOrj2_fawWglET8Yl69DHfvmw24TTBTs2ZQ8rU11tk9fzswGQHaIva7JAy6Lc9K-pYveDR6E2Gk2WfFp0Ct_B_HiDa2KGGXwAAAAH-pi3FAA")
 MONGODB_URI    = os.getenv("MONGODB_URI", "mongodb+srv://TeleVault:TeleVault@televault.6kr2f9n.mongodb.net/?appName=TeleVault")
 # ── Optional ──────────────────────────────────────────────────────────────────────
 LOGGER_ID = os.getenv("LOGGER_ID", None)
