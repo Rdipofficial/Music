1126ab666f2573e08f61de28e6dfdd5e8d5ecab6
Rdip <96578868+Rdipofficial@users.noreply.github.com>
2026-07-23 00:46:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,7 @@ GROUP       = os.getenv("GROUP", "nub_coder_s")
 # ── Sensitive — must be set via environment, no defaults ────────────────────────
 BOT_TOKEN      = os.getenv("BOT_TOKEN", "8975766165:AAENEio15whYE0FmOObR8xVlFZ1x1hNsMto")
 STRING_SESSION = os.getenv("STRING_SESSION", "BQFGMU4AQYjaTl5KGFywai_fk4ILzgKy0h66BURPla6j92kyov_G2WtBBcKmzfdETS9fiQHM5QKc3J5azp16PGsyCiFu1wXvVJIE_ln4xHfQxxFr-cVku47xSMSYW4jHakdXZbqF2i_ia92nfNEt4vUXg80Hvh5rB76qwokhOIsd6YnpCUkfoa7700FgROyc0k1clNlD373JXGwVk9r3LhbbXws7aS67zdppk8k2rpTnxcJFewKUcTO3ztMQZffSFszodsxgJArLlWObrU_vuO5wnv366nj267zJdqThLrl_6mirIvSDpIegjGVoIa6130ZwS2t_-cdf-GZGV72O2Bjf-UxMygAAAAH-pi3FAA")
-MONGODB_URI    = os.getenv("MONGODB_URI", "mongodb+srv://nubcoders:nubcoders@music.8rxlsum.mongodb.net/?retryWrites=true&w=majority&appName=music")
-
+MONGODB_URI    = os.getenv("MONGODB_URI", "mongodb+srv://TeleVault:TeleVault@televault.6kr2f9n.mongodb.net/?appName=TeleVault")
 # ── Optional ──────────────────────────────────────────────────────────────────────
 LOGGER_ID = os.getenv("LOGGER_ID", None)
 DB_NAME   = os.getenv("DB_NAME", "musicbot")
